820b9515b30bec05f68a4c8c5a39099b0edf06ef
Tina Gosarič <tina.gosaric@gorenje.com>
2022-12-01 08:59:24 +0100

    Matcher and Group methods

--- src/com/tg/Main.java
+++ src/com/tg/Main.java
@@ -7,16 +7,28 @@ import java.util.zip.CheckedOutputStream;
 public class Main {
     public static void main(String[] args) {
         StringBuilder htmlText = new StringBuilder("<h1>My Heading</h1>");
-        htmlText.append("<h2>Sub-heading<h2>");
+        htmlText.append("<h2>Sub-heading</h2>");
         htmlText.append("<p>This is a paragraph about something.</p>");
         htmlText.append("<p>This is another paragraph about something else.</p>");
         htmlText.append("<h2>Summary</h2>");
         htmlText.append("<p>Here is the summary.</p>");
 
-        String h2Pattern = ".*<h2>.*"; // . will match any character and * means zero or more (there can be anything before and anything after <h2>)
-        Pattern pattern = Pattern.compile(h2Pattern);
-        Matcher matcher = pattern.matcher(htmlText);
-        System.out.println(matcher.matches());
+        String h2GroupPattern = "(<h2>.*?</h2>)"; // ? turns greedy * into lazy one
+        Pattern groupPattern = Pattern.compile(h2GroupPattern);
+        Matcher groupMatcher = groupPattern.matcher(htmlText);
+        System.out.println(groupMatcher.matches());
+        groupMatcher.reset();
 
+        while (groupMatcher.find()) {
+            System.out.println("Occurrence: " + groupMatcher.group(1));
+        }
+
+        String h2TextGroups = "(<h2>)(.+?)(</h2>)";
+        Pattern h2TextPattern = Pattern.compile(h2TextGroups);
+        Matcher h2TextMatcher = h2TextPattern.matcher(htmlText);
+
+        while (h2TextMatcher.find()) {
+            System.out.println("Occurence: " + h2TextMatcher.group(2)); // we have 3 groups, group 2 represents the text between the tags
+        }
     }
 }
